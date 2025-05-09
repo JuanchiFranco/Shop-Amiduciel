7be89e8f7a1a5607e124bfb3c7df4b522e5b578b
Juan Franco Vargas <francovargasjuandario@gmail.com>
2025-05-09 09:54:35 -0500

    add paragraph to news section

--- amiduciel-front/src/pages/NewsSection.jsx
+++ amiduciel-front/src/pages/NewsSection.jsx
@@ -33,10 +33,13 @@ const NewsSection = () => {
     return (
         <section id="news">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6" >
-                <div className="flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold text-gray-800 text-center md:text-left">
                         Recién Agregados
                     </h1>
+                    <p className="text-gray-600 text-center md:text-left mt-2">
+                        Descubre los productos más recientes que hemos añadido a nuestra colección. ¡No te los pierdas!
+                    </p>
                 </div>
                 <div 
                     className="flex flex-wrap justify-center gap-5 bg-cover bg-center p-10" 
